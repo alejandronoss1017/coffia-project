940d6214b56b437868dd299c912458981a1d648a
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-09 16:52:15 -0500

    Componentes de iconos y sus tipos agregados

---
+++ app/types/IconsProps.ts
@@ -0,0 +1,12 @@
+export type IconsProps = {
+  width: string;
+  height: string;
+  fill: string;
+};
+
+export type LinkedInProps = {
+  width: string;
+  height: string;
+  backgroundFill: string;
+  contentFill: string;
+};
