2b6109d0f7c1d5ee52c36880c99c0b262de2bdd1
Camilo <alejandro_noss@hotmail.com>
2023-05-15 11:32:56 -0500

    Nueva configuración nextConfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  Images: {
-    loader: 'akamai',
-    path: '/'
-  }
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
