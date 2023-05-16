b5817427fd2d0d7fbb36120b81428039599b35fb
Camilo <alejandro_noss@hotmail.com>
2023-05-15 19:08:23 -0500

    Checkout Page creada

---
+++ app/shop/checkout/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  );
+};
+
+export default CheckoutPage;
