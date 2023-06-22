07320f6925547ee968967a1cc4c3ea3a52e7aaed
Camilo Nossa <calejandro_noss@outlook.com>
2023-06-22 15:12:20 -0500

    Informacion de los clientes añadida

---
+++ app/data/clientsData.ts
@@ -0,0 +1,22 @@
+import { Client } from '../types/Client';
+
+export const clientsData: Client[] = [
+  {
+    name: 'FedeCafe',
+    logo: '/FedeCafe.png',
+    logoAlt: 'FedeCafe',
+    website: 'https://federaciondecafeteros.org/wp/'
+  },
+  {
+    name: 'Universidad Javeriana',
+    logo: '/Javeriana.png',
+    logoAlt: 'Universidad Javeriana',
+    website: 'https://www.javeriana.edu.co/inicio'
+  },
+  {
+    name: 'Tostao',
+    logo: '/TostaoLogo.webp',
+    logoAlt: 'Tostao',
+    website: 'https://nosotros.tostao.com/'
+  }
+];
